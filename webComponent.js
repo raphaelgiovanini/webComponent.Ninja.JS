2a549483bf9f44a107a68d0834812791b85d4945
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2015-09-14 13:59:05 +0000

    refatoracao

--- webComponent.js
+++ webComponent.js
@@ -1,20 +1,19 @@
 this.Ninja.module('$webComponent', [
   
+  '$always',
   '$apply',
   '$concat',
   '$curry',
   '$event',
   '$split'
 
-], function ($apply, $concat, $curry, $event, $split) {
+], function ($always, $apply, $concat, $curry, $event, $split) {
   
-  function stub() {}
-
   return function (name, description) {
     
     function eventDelegation(root) {
       for (var key in description.events || {}) {
-        $apply($event((root.shadowRoot || root)).delegation, $concat($split(key, ' '), [description.events[key].bind(null, root)]));
+        $apply($event(root.shadowRoot).delegation, $concat($split(key, ' '), [description.events[key].bind(null, root)]));
       }
     }
     
@@ -24,40 +23,36 @@ this.Ninja.module('$webComponent', [
       }
     }
     
-    function render(root, html) {
-      (root.shadowRoot || root).innerHTML = html;
-    }
-    
     document.registerElement(name, {
       prototype: Object.create(HTMLElement.prototype, {
       
         attachedCallback: {
           value: function () {
-            (description.attached || stub)(this);
+            (description.attached || $always())(this);
           }
         },
       
         attributeChangedCallback: {
           value: function (attrName, oldValue, newValue) {
-            ((description.attributes || {})[attrName] || stub)(this, oldValue, newValue);
+            ((description.attributes || {})[attrName] || $always())(this, oldValue, newValue);
           }
         },
         
         createdCallback: {
           value: function () {
-            (this.createShadowRoot && this.createShadowRoot()), eventDelegation(this), merge(this), (description.created || stub)(this);
+            merge(this), this.createShadowRoot(), eventDelegation(this), (description.created || $always())(this);
           }
         },
         
         detachedCallback: {
           value: function () {
-            (description.detached || stub)(this);
+            (description.detached || $always())(this);
           }
         },
         
         setState: {
           value: function (data) {
-            (description.template || stub)(this, data, $curry(render)(this));
+            (description.template || $always())(this, data);
           }
         }
         
